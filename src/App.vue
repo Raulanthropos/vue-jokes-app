cb1615e5a169a9bff0ea606227758e811cec91b9
Raulanthropos <ipsichias@yandex.com>
2025-03-27 18:49:34 +0200

    Added tailwind config to fix class purging

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,6 @@ import HelloWorld from './components/HelloWorld.vue'
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
-    <p class="text-red-600 text-2xl font-bold text-center">Tailwind is alive! 🎯</p>
-
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
